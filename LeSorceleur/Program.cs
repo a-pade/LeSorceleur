8b84b8b51f17ba1344a9e89a84875b72f2aab1cd
apade17 <antoine62217a@gmail.com>
2021-10-22 10:30:56 +0200

    Classes Sans Combat

--- LeSorceleur/Program.cs
+++ LeSorceleur/Program.cs
@@ -5,11 +5,18 @@ namespace LeSorceleur
 {
     class Program
     {
-
+        
         static void Main(string[] args)
         {
-          
+
+            Personnage player1 = new Personnage();
+            Personnage player2 = new Personnage();
+
             
+            player1.AffichageDonnees();
+            player2.AffichageDonnees();
         }
+
+        
     }
 }
